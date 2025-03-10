9e0c0c6271e676bc48e5be46040c199b3ec56ec3
Vijayaragupathy Vijayaragunathapandian <157554913+Vvijayaragupathy-uno@users.noreply.github.com>
2025-03-10 13:31:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 from llm import load_and_split_document, create_vector_store, rag_with_groq, setup_rag
 from langchain_groq import ChatGroq 
 from visualization import visualization_page
-from userauth import admin_login, is_admin, admin_login_page
 
 
 GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
